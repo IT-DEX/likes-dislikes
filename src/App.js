37d968b7db8d4d258ef834ab11167d3dcd5feb9c
IT-DEX <v.v.ryabova@gmail.com>
2022-05-02 23:55:53 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Likes } from './Likes';
+
+function App() {
+  return (
+    <div>
+      <Likes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Likes.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+export class Likes extends Component {
+
+constructor() {
+    super();
+
+    this.state = {
+        likes: 300
+    }
+}
+
+updateLikes() {
+    this.setState({likes: this.state.likes + 1})
+}
+
+minusLikes() {
+    this.setState({likes: this.state.likes - 1})
+}
+
+render() {
+    return (
+        <div>
+            <h1>{this.state.likes} LIKES</h1>
+            <button onClick={() => this.updateLikes()}>Like</button>
+            <button onClick={() => this.minusLikes()}>Dislike</button>
+        </div>
+    )
+}
+}
